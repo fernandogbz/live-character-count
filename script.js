242552cfe09797922b2d0d2529ea7dc88eb3452f
Fernando Jimenez <fgjv06@gmail.com>
2023-03-06 18:42:14 -0300

    JS - total characters added to document

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myText = document.getElementById("my-text");
 myText.addEventListener("input", () => {
   let count = (myText.value).length;
-  console.log(myText.value, count)
+  console.log(myText.value, count);
+  document.getElementById("count-result").textContent = `Total characters : ${count}`;
 })
